78a4dc64ff68106f77ffc78e0618b2ff089e159a
Mike <mbarton.mail@gmail.com>
2014-07-07 16:38:13 -0700

    Adding new links properly connects to nodes

--- src/graph.js
+++ src/graph.js
@@ -5,14 +5,14 @@ var Graph = function(){
 
 Graph.prototype.addVertex = function(vert){
 	if (!this.contains(vert)){
-		this.nodes.push({"name": vert, "group": 1})
+		this.nodes.push({"id": vert, "group": 1})
 	}
 };
 
 Graph.prototype.findIndex = function(vert){
 	var index = -1;
 	for (var i = 0; i < this.nodes.length; i++){
-		if (this.nodes[i].name === vert) return i;
+		if (this.nodes[i].id === vert) return i;
 	}
 	return index;
 }
@@ -30,7 +30,7 @@ Graph.prototype.addEdge = function(from, to){
 Graph.prototype.contains = function(vert){
   var found = false;
   for (var i = 0; i < this.nodes.length; i++){
-  	if (this.nodes[i].name === vert){
+  	if (this.nodes[i].id === vert){
   		found = true
   	}
   }
--- src/graphVisualization.js
+++ src/graphVisualization.js
@@ -1,61 +1,89 @@
+graph = new Graph()
+
 var width = 960,
     height = 500;
 
 var color = d3.scale.category20();
 
 var force = d3.layout.force()
-    .charge(-120)
-    .linkDistance(30)
-    .size([width, height]);
+      .nodes(graph.nodes) ///graph is where the data comes from
+      .links(graph.links)
+      .charge(-90)
+      .linkDistance(90)
+      .size([width, height]);
 
 var svg = d3.select("body").append("svg")
     .attr("width", width)
     .attr("height", height);
 
-graph = new Graph()
 graph.addVertex("bob");
 graph.addVertex("mike");
 graph.addEdge("bob", "mike");
 console.log(graph.nodes)
 console.log(graph.links)
 
-force
-    .nodes(graph.nodes) ///graph is where the data comes from
-    .links(graph.links)
-    .start();
-
-var link = svg.selectAll(".link")
-    .data(graph.links)
-    .enter().append("line")
-      .attr("class", "link")
-      .style("stroke-width", function(d) { return Math.sqrt(d.value); });
-
-var node = svg.selectAll(".node")
-    .data(graph.nodes)
-    .enter().append("circle")
-      .attr("class", "node")
-      .attr("r", 5)
-      .style("fill", function(d) { return color(d.group); })
-      .call(force.drag);
-
-node.append("title")
-    .text(function(d) { return d.name; });
-
-force.on("tick", function() {
-  link.attr("x1", function(d) { return d.source.x; })
-      .attr("y1", function(d) { return d.source.y; })
-      .attr("x2", function(d) { return d.target.x; })
-      .attr("y2", function(d) { return d.target.y; });
-
-  node.attr("cx", function(d) { return d.x; })
-      .attr("cy", function(d) { return d.y; });
-});
-
 var update = function(){
+
+
+
+  var node = svg.selectAll(".node")
+      .data(graph.nodes)
+      
+  node.exit().transition().duration(100).remove()
+
+  var nodeEnter = node.enter().append("circle")
+        .attr("class", "node")
+        .attr("r", 6)
+        .style("fill", function(d) { return color(d.group); })
+        .call(force.drag);
+
+  nodeEnter.append("title")
+      .text(function(d) { return d.name; });
+
+  var link = svg.selectAll(".link")
+      .data(force.links(), function(d){ return d.source.index + "-" + d.target.index; });
+
+  var linkEnter = link.enter().insert("line", ".node")
+        .attr("class", "link")
+        .style("stroke-width", function(d) { return Math.sqrt(d.value); });
+
+  link.exit().transition().duration(100).remove()
   
+  force.start();
+
+  force.on("tick", function() {
+    link.attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
+
+    node.attr("cx", function(d) { return d.x; })
+        .attr("cy", function(d) { return d.y; });
+  });
+
+
 }
 
+//only for testing.  No need to update blank graph
+update()
 
+var addEdgeInput = function(){
+ if(event.which == 13) {
+        event.preventDefault();
+        console.log('adding edge')
+        var a = $("#addEdgeInputA").val();
+        var b = $("#addEdgeInputB").val();
+          if (graph === undefined || !graph.contains(a) || !graph.contains(b)){
+            alert("NYOOOPE")
+          } else {
+              graph.addEdge(a, b);
+              update();
+            }
+        $(this).val("")
+    }
+}
+
+//Add a vertex via addVertexInput field
 $("#addVertexInput").on("keypress", function(event) {
     if(event.which == 13) {
         event.preventDefault();
@@ -65,7 +93,10 @@ $("#addVertexInput").on("keypress", function(event) {
             graph = new Graph();
           }
           graph.addVertex(val);
-          //update();
+          update();
         $(this).val("")
     }
 });
+
+$("#addEdgeInputA").on("keypress", addEdgeInput);
+$("#addEdgeInputB").on("keypress", addEdgeInput);
