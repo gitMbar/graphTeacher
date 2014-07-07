2b25ddb61c0ce9bef0f0acc3f0484331c2ec1e1a
Mike <mbarton.mail@gmail.com>
2014-07-07 11:54:11 -0700

    Add addVertex and contains method to graph.js

--- graph.js
+++ graph.js
@@ -1,1 +1,23 @@
-graph.js
+var Graph = function(){
+	this.storage = {"nodes": [], "links": []};
+};
+
+Graph.prototype.addVertex = function(vert){
+	if (!this.contains(vert)){
+		this.storage.nodes.push({"name": vert, "group": 1})
+	}
+};
+
+Graph.prototype.addEdge = function(from, to){
+
+};
+
+Graph.prototype.contains = function(vert){
+  var found = false;
+  for (var i = 0; i < this.storage.nodes.length; i++){
+  	if (this.storage.nodes[i].name === vert){
+  		found = true
+  	}
+  }
+  return found;
+};
