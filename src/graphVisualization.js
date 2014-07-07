c16055cb36b229b4d2f5face06262fe631df65bf
Mike <mbarton.mail@gmail.com>
2014-07-07 15:02:53 -0700

    Add input functionality for addVertex

--- src/graphVisualization.js
+++ src/graphVisualization.js
@@ -18,51 +18,54 @@ graph.addVertex("mike");
 graph.addEdge("bob", "mike");
 console.log(graph.nodes)
 console.log(graph.links)
-  force
-      .nodes(graph.nodes) ///graph is where the data comes from
-      .links(graph.links)
-      .start();
 
-  var link = svg.selectAll(".link")
-      .data(graph.links)
+force
+    .nodes(graph.nodes) ///graph is where the data comes from
+    .links(graph.links)
+    .start();
+
+var link = svg.selectAll(".link")
+    .data(graph.links)
     .enter().append("line")
       .attr("class", "link")
       .style("stroke-width", function(d) { return Math.sqrt(d.value); });
 
-  var node = svg.selectAll(".node")
-      .data(graph.nodes)
+var node = svg.selectAll(".node")
+    .data(graph.nodes)
     .enter().append("circle")
       .attr("class", "node")
       .attr("r", 5)
       .style("fill", function(d) { return color(d.group); })
       .call(force.drag);
 
-  node.append("title")
-      .text(function(d) { return d.name; });
+node.append("title")
+    .text(function(d) { return d.name; });
 
-  force.on("tick", function() {
-    link.attr("x1", function(d) { return d.source.x; })
-        .attr("y1", function(d) { return d.source.y; })
-        .attr("x2", function(d) { return d.target.x; })
-        .attr("y2", function(d) { return d.target.y; });
+force.on("tick", function() {
+  link.attr("x1", function(d) { return d.source.x; })
+      .attr("y1", function(d) { return d.source.y; })
+      .attr("x2", function(d) { return d.target.x; })
+      .attr("y2", function(d) { return d.target.y; });
 
-    node.attr("cx", function(d) { return d.x; })
-        .attr("cy", function(d) { return d.y; });
-  });
+  node.attr("cx", function(d) { return d.x; })
+      .attr("cy", function(d) { return d.y; });
+});
+
+var update = function(){
+  
+}
 
 
-
-var addVertexButton = document.getElementById('addVertex')
-
-/*addVertexButton.onkeydown = function(event) {
+$("#addVertexInput").on("keypress", function(event) {
     if(event.which == 13) {
         event.preventDefault();
-          if (basicGraph === undefined){
-            basicGraph = new Graph();
+        console.log('hisf')
+        val = $(this).val();
+          if (graph === undefined){
+            graph = new Graph();
           }
-          basicGraph.addVertex("test");
-          update();
-        }
+          graph.addVertex(val);
+          //update();
         $(this).val("")
     }
-});*/
+});
